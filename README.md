5faa2e63bb16c3ee686fc270efed0a5e621faad8
hasnain-cedcoss <syedhasnainmehadi@cedcommerce.com>
2022-12-27 16:49:02 +0530

    improvement on setup

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# node-simple-server
+# node-server-setup
+
+### create .env file on root put these variable
+
+```js
+NODE_ENV = development;
+PORT = "";
+MONGO_URI = "";
+JWT_SECRET = "";
+EMAIL_ID = "";
+EMAIL_PASS = "";
+```
--- src/services/email.js
+++ src/services/email.js
@@ -1,1 +1,32 @@
-export const registrationMail = () => {};
+import chalk from "chalk";
+import dotenv from "dotenv";
+import nodemailer from "nodemailer";
+import { signUp } from "../../public/templates/email.js";
+dotenv.config();
+
+let transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: process.env.EMAIL_ID,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    ciphers: "SSLv3",
+    rejectUnauthorized: false,
+  },
+});
+
+export const registrationMail = async (data) => {
+  try {
+    const info = await transporter.sendMail({
+      from: process.env.EMAIL_ID,
+      to: data?.email,
+      subject: "Account Confirmation Mail",
+      text: "Account Confirmation Mail",
+      html: signUp(data),
+    });
+    console.log(chalk.red("sent email id:", info.messageId));
+  } catch (error) {
+    console.log(chalk.bgRed.bold(error));
+  }
+};
