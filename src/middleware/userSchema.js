cf084e018b20013da0216c5713522052df0782ef
hasnain-cedcoss <syedhasnainmehadi@cedcommerce.com>
2023-03-21 15:19:49 +0530

    worked on auth route

--- src/middleware/userSchema.js
+++ src/middleware/userSchema.js
@@ -3,20 +3,15 @@ import * as yup from 'yup';
 export const userSchema = yup.object({
   body: yup.object({
     email: yup.string().email().required(),
-    username: yup.string().min(4).max(10).required(),
-    name: yup.string().min(4).max(18).required(),
-    password: yup.string().min(4).max(8).required()
-  }),
-  header: yup.object({
-    authorization: yup.string().required()
+    name: yup.string().min(4).max(30).required(),
+    password: yup.string().min(4).max(10).required()
   })
 });
 
 export const validate = (schema) => async (req, res, next) => {
   try {
     await schema.validate({
-      body: req.body,
-      header: req.headers
+      body: req.body
     });
     return next();
   } catch (err) {
